883bc2845511333058e6ccd21330ac513a745fad
Darya <darapravednikova@gmail.com>
2025-08-09 17:00:17 +0400

    UPD extract header component (with search input) & ger markers js mock api

--- web-client/web-app/src/App.tsx
+++ web-client/web-app/src/App.tsx
@@ -1,20 +1,38 @@
 import MapComponent from './map.tsx';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HeaderComponent from './header.tsx';
+import { getMarkers } from './GetMarkes.ts';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchValue, setSearchValue] = useState('');
+  const [markers, setMarkers] = useState([]);
+
+  useEffect(() => {
+      // Загружаем маркеры асинхронно
+      const fetchMarkers = async () => {
+          try {
+              const data = await getMarkers();
+              setMarkers(data);
+          } catch (error) {
+              console.error('Ошибка загрузки маркеров:', error);
+              setMapError('Не удалось загрузить маркеры');
+          }
+      };
+
+      fetchMarkers();
+  }, []);
 
   return (
     <>
-      
-      
-      <MapComponent>
-       
+      <HeaderComponent onSearchInputChange={setSearchValue} markers={markers} />
+
+      <MapComponent markers={markers}>
+
       </MapComponent>
-     
+
     </>
   )
 }
--- web-client/web-app/src/map.tsx
+++ web-client/web-app/src/map.tsx
@@ -2,7 +2,9 @@ import React, { useRef, useEffect, useState } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './MapComponent.css';
-const MapComponent = () => {
+
+const MapComponent = ({ markers = [] }) => {
+    console.log(markers);
     const mapContainer = useRef(null);
     const map = useRef(null);
     const [mapError, setMapError] = useState(null);
@@ -14,35 +16,6 @@ const MapComponent = () => {
     }
 
 
-    const markers = [
-        {
-            id: 1,
-            url: "http: //example",
-            coordinates: [37.6178, 55.7512],
-            adress: "Остановка",
-            count: 5,
-            velocity: 5,
-            load: 10,
-        },
-        {
-            id: 2,
-            coordinates: [30.3358, 59.9342],
-            url: "http: //example",
-            adress: "Государственный Эрмитаж",
-            count: 5,
-            velocity: 5,
-            load: 5,
-        },
-        {
-            id: 3,
-            url: "http: //example",
-            coordinates: [43.5855, 39.7231],
-            title: "Олимпийский парк Сочи",
-            count: 5,
-            velocity: 5,
-            load: 1,
-        }
-    ];
     useEffect(() => {
         if (!mapContainer.current) {
             setMapError('Контейнер карты не найден');
@@ -158,7 +131,7 @@ const MapComponent = () => {
         return () => {
             if (map.current) map.current.remove();
         };
-    }, []);
+    }, [markers]);
 
     return (
         <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
