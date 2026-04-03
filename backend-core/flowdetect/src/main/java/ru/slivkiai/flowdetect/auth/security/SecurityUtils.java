3ceb7bd6b599fd4a4bc08e86f2534e8c5d2104bc
Nikbelini <mikabel2004@mail.ru>
2026-04-03 15:23:39 +0400

    dev mini fix security

--- backend-core/flowdetect/src/main/java/ru/slivkiai/flowdetect/auth/security/SecurityUtils.java
+++ backend-core/flowdetect/src/main/java/ru/slivkiai/flowdetect/auth/security/SecurityUtils.java
@@ -1,4 +1,4 @@
-package ru.slivkiai.flowdetect.auth.utils;
+package ru.slivkiai.flowdetect.auth.security;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
